f23bce53268bad9a3e67e32e8b11ff2e32e1d514
Matt <154559729+davis-matt0@users.noreply.github.com>
2024-01-18 15:50:24 -0500

    add mouseover function

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const container = document.querySelector("#container");
+const cells = document.querySelectorAll(".cells");
+const reset = document.querySelector(".reset");
 
 function makeDivs(numDivs) {
   for (let i = 0; i < numDivs; i++) {
     let cells = document.createElement("div");
     container.appendChild(cells);
+    cells.setAttribute("class", "cells");
+    cells.addEventListener("mouseover", () => {
+      cells.setAttribute("style", "background-color: black");
+    });
   }
 }
+
+reset.addEventListener("click", () => {});
 
 makeDivs(256);
